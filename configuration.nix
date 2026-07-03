78243126b22f5f10a271651a384f4dae1ceeb19e
8nue <boyog@icloud.com>
2026-07-03 22:53:02 +0000

    nix file less ugly

--- configuration.nix
+++ configuration.nix
@@ -1,148 +1,96 @@
 { config, lib, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+	imports =
+		[ # Include the results of the hardware scan.
+			./hardware-configuration.nix
+		];
 
-################
-# system stuff #
-################
+	################
+	# system stuff #
+	################
 
-boot.loader.systemd-boot.enable = true;
-boot.loader.efi.canTouchEfiVariables = true;
-networking.hostName = "nixos";
-networking.networkmanager.enable = true; #nmtui goat
+	boot.loader.systemd-boot.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+	networking.hostName = "nixos";
+	networking.networkmanager.enable = true; #nmtui goat
+	nixpkgs.config.allowUnfree = true;
 
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
-# god knows what this does
-nixpkgs.config.allowUnfree = true;
+	###################
+	# display & audio #
+	###################
 
-  programs.hyprland = {
-  	enable = true;
-	xwayland.enable = true;
-};
-  
-hardware.graphics.enable = true;
-services.xserver.videoDrivers = [ "nvidia" ];
+	programs.hyprland = {
+		enable = true;
+		xwayland.enable = true;
+	};
+	hardware.graphics.enable = true;
+	services.xserver.videoDrivers = [ "nvidia" ];
+	hardware.nvidia = {
+		modesetting.enable = true;
+		powerManagement.enable = false;
+		open = false;
+		nvidiaSettings = true;
+		package = config.boot.kernelPackages.nvidiaPackages.stable;
+	};
 
-hardware.nvidia = {
-	modesetting.enable = true;
-	powerManagement.enable = false;
-	open = false;
-	nvidiaSettings = true;
-	package = config.boot.kernelPackages.nvidiaPackages.stable;
-};
+	environment.sessionVariables = {
+		NIXOS_OZONE_WL = "1";
+		GBM_BACKEND = "nvidia-drm";
+		__GLX_VENDOR_LIBRARY_NAME = "nvidia";
+	};
+	programs.gamemode.enable = true;
+	services.pipewire = {
+		enable = true;
+		pulse.enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		jack.enable = true;
+	};
 
-environment.sessionVariables = {
-	NIXOS_OZONE_WL = "1";
-	GBM_BACKEND = "nvidia-drm";
-	__GLX_VENDOR_LIBRARY_NAME = "nvidia";
-};
-  
-programs.steam = {
-	enable = true;
-};
+	#################	
+	# user settings #
+	#################
+	
+	users.users.slushy = {
+		isNormalUser = true;
+		extraGroups = [ "wheel" ];
+		packages = with pkgs; [
+			tree
+			fastfetch
+			alacritty
+			thunar
+			firefox
+			neovim
+			nix-search-cli
+			prismlauncher
+			vencord
+			rofi
+			lutris
+			steam
+			protonup-qt
+			waybar
+			hyprpaper
+			bottles
+		];
+		initialPassword = "changeme";
+	};
 
-programs.gamemode.enable = true;
+	environment.systemPackages = with pkgs; [
+		nano
+		wget
+		curl
+		git
+		stdenv
+		htop
+		gnumake
+		github-cli
+	];
 
-   services.pipewire = {
-     enable = true;
-     pulse.enable = true;
-     alsa.enable = true;
-     alsa.support32Bit = true;
-     jack.enable = true;
-   };
+	fonts.packages = with pkgs; [
+		nerd-fonts.jetbrains-mono
+	];
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-
-   users.users.slushy = {
-     isNormalUser = true;
-     extraGroups = [ "wheel" ];
-     packages = with pkgs; [
-       tree
-	fastfetch
-	alacritty
-	thunar
-	firefox
-	neovim
-	nix-search-cli
-	prismlauncher
-	vencord
-	rofi
-	lutris
-	steam
-	protonup-qt
-	waybar
-	hyprpaper
-	bottles
-     ];
-     initialPassword = "changeme";
-   };
-
-  # programs.firefox.enable = true;
-
-  # List packages installed in system profile.
-  # You can use https://search.nixos.org/ to find more packages (and options).
-environment.systemPackages = with pkgs; [
-	nano # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-	wget
-	curl
-	git
-	stdenv
-	htop
-	gnumake
-	github-cli
-   ];
-
-fonts.packages = with pkgs; [
-	nerd-fonts.jetbrains-mono
-];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
-
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
-  # to actually do that.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "26.05"; # Did you read the comment?
+	system.stateVersion = "26.05"; # dont change this probably
 
 }
